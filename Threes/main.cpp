7095538aa30805ed02caf1890667ff8a3152cb8f
Eric <ericjang@kimo.com>
2018-09-30 21:14:03 +0800

    Initial Commit

---
+++ Threes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Threes
+//
+//  Created by Eric Cheng on 9/30/18.
+//  Copyright © 2018 Eric Cheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
